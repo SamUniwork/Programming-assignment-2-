920fb3094506347db04f03a147d40eafd9430c39
SamUniwork <157488297+SamUniwork@users.noreply.github.com>
2024-05-12 13:18:12 +0100

    Add files via upload
    here is the first part of the real version of my program it shows just the class and using get and set for it

---
+++ Github Repositories Class1/Github Repositories/Program.cs
@@ -0,0 +1,11 @@
+﻿class TheItems
+{//this class here shows the items which is avaiable for renting 
+ //it has a constructor which will initiate the item and the name and its availability
+    public string Name { get; }
+    public bool Available { get; set; }
+
+    public TheItems(string customername)
+    {
+        Name = customername;
+        Available = true;
+    }
