0f206ba02272f33c8579394162d8c13c54dcd5f7
SamUniwork <157488297+SamUniwork@users.noreply.github.com>
2024-05-12 13:25:35 +0100

    Add files via upload

---
+++ Github Repositories Class2.cs
@@ -0,0 +1,12 @@
+﻿class Customer
+{//this class here shows the customer renting the item
+ // the constructor will initiate the customer with the name and the email as well
+    public string Name { get; }
+    public string Email { get; }
+
+    public Customer(string customername, string customeremail)
+    {
+        Name = customername;
+        Email = customeremail;
+    }
+}
